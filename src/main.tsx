fbae1bf75782c89865abb1c449d288b9d3d164d7
Patryk Znamirowski <znamirowskipatryk@gmail.com>
2024-12-11 20:22:51 +0100

    tal

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter as ReactRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <ReactRouter>
       <App />
-    </BrowserRouter>
+    </ReactRouter>
   </StrictMode>
 );
